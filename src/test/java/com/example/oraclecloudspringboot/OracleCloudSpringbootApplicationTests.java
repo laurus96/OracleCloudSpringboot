5e757d51e69a715284a8dbe6e29b2779902fbaa9
Lorenzo Paolo Cocchinone <pallinadiriso.zp92a@simplelogin.com>
2022-11-07 11:31:59 +0100

    Initial Commit and Datasource Property Added

---
+++ src/test/java/com/example/oraclecloudspringboot/OracleCloudSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oraclecloudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleCloudSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
